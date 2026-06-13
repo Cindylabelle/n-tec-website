83e4e6f7fa88136739438165dada15c9ebdf32fe
Cindylabell <ndzilabell12@gmail.com>
2026-06-13 22:26:22 +0100

    fix: connect application form to Formspree

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default function NtecHome() {
     // TODO: Replace with your actual Formspree endpoint
     // Example: https://formspree.io/f/YOUR_FORM_ID
     try {
-      const response = await fetch('https://formspree.io/f/YOUR_FORM_ID', {
+      const response = await fetch('https://formspree.io/f/xnjylapr', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
